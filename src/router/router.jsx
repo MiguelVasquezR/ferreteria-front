32120b0cf6d7f359e24f8aeb24b5fe44a09ae778
MiguelVasquezR <mvasquesrosas@gmail.com>
2024-09-16 17:58:21 -0600

    its ready for work

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import ViewMain from "../view/viewMain/ViewMain.jsx";
 import ViewLogin from "../view/viewLogin/ViewLogin.jsx";
 import ViewForgetPassword from "../view/viewForgetPassword/ViewForgetPassword.jsx";
 import ViewForgetPasswordAction from "../view/viewForgetPassword/viewForgetPasswordAction/ViewForgetPasswordAction.jsx";
+import ProtectedRoute from "./ProtecterRoute.jsx";
+
+/*
+  Según la lógica de negocio, tendremos 3 interfaces diferentes segun los roles, entonces debemos de validar
+  que el usuario quiere ingresar al sistema tenga el permiso de entrar a esa pestalla. He creado el componente 
+  ProtectedRouter que se encarga de validar si existen token y rol en nuestro localstorage, si no exiten este se encarga
+  de redirigir al login, pero si los tiene, valida que el rol almacenado en nuestrol local sea igual al rol requerido
+  
+  pd -> En el caso de que no se requiera un rol, solo poner el componente dentro de la prop element
+
+*/
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+  },
+  {
+    path: "*",
+    element: <div>Ruta no encontrada</div>,
   },
   {
     path: "/login",
@@ -24,12 +38,12 @@ const router = createBrowserRouter([
   },
   {
     path: "/home",
-    element: <ViewMain />,
+    element: <ProtectedRoute element={<ViewMain />} requiredRole="admin" />,
   },
 ]);
 
-const Router = () => {
+const AppRouter = () => {
   return <RouterProvider router={router} />;
 };
 
-export default Router;
+export default AppRouter;
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import proveedor_reducers from "./slices/proveedor/proveedor_reducers";
-import photo_reducers from "./slices/photo/photo_reducer";
 
 export const store = configureStore({
   reducer: {
     proveedores: proveedor_reducers,
-    photo: photo_reducers,
   },
 });
