9272789fbb35001e6b19e999c62a4c599eb7725e
Karen Yoval <karenyoval26@gmail.com>
2024-11-04 18:51:50 -0600

    FixOfferProduct

--- src/view/viewOfferProduct/ViewOfferProduct.jsx
+++ src/view/viewOfferProduct/ViewOfferProduct.jsx
@@ -2,7 +2,7 @@ import Header from "../../components/Header/Header";
 import { IoArrowBackOutline } from "react-icons/io5";
 import { FormProvider, useForm } from "react-hook-form";
 import Card from "../../components/cardAddPackage/cardAddPackage";
-
+import TextField from "../../components/Form/TextField/TextField";
 const ViewOfferProduct = () => {
   const methods = useForm();
 
@@ -40,39 +40,23 @@ const ViewOfferProduct = () => {
 
       <form className="flex flex-col items-center mt-5 w-full max-w-[900px] px-4 md:px-40">
         <FormProvider {...methods}>
-          <div className="flex flex-col gap-5 w-full mt-4">
-            <div className="flex flex-col w-full">
-              <label htmlFor="nombre" className="pl-2 mb-1 text-lg">
-                Precio
-              </label>
-              <input
-                id="nombre"
-                type="text"
-                {...methods.register("nombre", {
-                  required: "Este campo es obligatorio",
-                })}
-                className={`shadow-md rounded-lg p-3 w-full outline-none bg-gradient-to-r from-gray-200 to-gray-300 border-none ${
-                  methods.formState.errors.nombre ? "border-red-500" : ""
-                }`}
-              />
-              {methods.formState.errors.nombre && (
-                <p className="text-red-500 text-sm pl-2">
-                  {methods.formState.errors.nombre.message}
-                </p>
-              )}
-            </div>
-
-            <div className="flex flex-col w-full">
-              <label htmlFor="precio" className="pl-2 mb-1 text-lg">
-                Fecha conclusión
-              </label>
-              <input
-                id="precio"
-                type="text"
-                className="shadow-md rounded-lg p-3 w-full outline-none bg-gradient-to-r from-gray-200 to-gray-300 border-none"
-              />
-            </div>
-          </div>
+          <TextField
+            label="Precio"
+            name="precio"
+            type="text"
+            register={methods.register}
+            Error={methods?.formState.errors?.nombre?.message}
+            isError={!!methods?.formState.errors?.nombre?.message}
+          />
+          <br />
+          <TextField
+            label="Fecha conclusión"
+            name="fecha"
+            type="date"
+            register={methods.register}
+            Error={methods?.formState.errors?.nombre?.message}
+            isError={!!methods?.formState.errors?.nombre?.message}
+          />
         </FormProvider>
       </form>
 
