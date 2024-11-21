833f0ed7d46e7fb245a1e639fb5d3f81c0297f25
MiguelVasquezR <mvasquesrosas@gmail.com>
2024-11-21 07:41:26 -0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 7890,
-    host: "0.0.0.0",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // Agrupa dependencias grandes como react y react-dom
+            if (id.includes("react")) {
+              return "vendor-react";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Opcional: aumenta el límite de advertencia (en kB)
   },
 });
